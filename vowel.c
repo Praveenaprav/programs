34c88045ad7051e57b7eb8c26df6cd6d1fce379e
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-01-17 16:52:22 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -6,7 +6,7 @@ char c;
 int l,u;
 printf("enter an alphabet:");
 scanf("%c",&c);
-l=(c=='a'||c=='e'||c=='i'||c='o'||c='u');
+l=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
 u=(c=='A'||c=='E'||c=='I'||c=='O'||c=='u');
 if(l||u)
 printf("%c is  a vowel",c);
