d56102e243fb303379f576493fd0b98290825362
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-01-17 16:14:51 +0530

    Create fac.c

---
+++ fac.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,fact=1;i;
+printf("enter the value of n:");
+scanf("%d",&n);
+fro(i=1;i<=n;i++)
+fact=fact*i;
+printf("the factorial  is %d:",fact);
+getch();
+}
