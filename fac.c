c999bb188d990ebfac7cdb0f66e5795e97b7ac8c
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-01-17 16:20:34 +0530

    Update fac.c

--- fac.c
+++ fac.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+int main()
 {
 int n,fact=1;i;
 printf("enter the value of n:");
 scanf("%d",&n);
-fro(i=1;i<=n;i++)
+for(i=1;i<=n;i++)
 fact=fact*i;
 printf("the factorial  is %d:",fact);
-getch();
+return 0;
 }
