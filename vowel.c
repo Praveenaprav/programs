881196b40e6ce319b3415ee8d16c55a1946167c8
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-01-17 16:43:10 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -9,7 +9,7 @@ scanf("%c",&n);
 isLowercaseVowel=(c=='a'||c=='e'||c=='i'||c='o'||c='u');
 isUppercaseVowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='u');
 if(isLowercaseVowel||isUppercaseVowel)
-printf("%c is  a vowel",n);
+printf("%c is  a vowel",c);
 else
-pritnf("%c is a consonent",n);
+pritnf("%c is a consonent",c);
 return 0;}
