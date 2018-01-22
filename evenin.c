2e566612b67bf6c596b504383bce1579459812bf
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-01-22 16:30:20 +0530

    Update evenin.c

--- evenin.c
+++ evenin.c
@@ -1,27 +1,16 @@
 #include<stdio.h> 
-
-int main(){
-
-           int f1,f2,rem,i;
-
-           printf("   Give the First number for the Range :   \n");
-
-           scanf("%d",&f1);
-
-           printf("   Give the Final number for the Range :   \n");
-
-           scanf("%d",&f2);
-
-           printf("\n The Even numbers between %d and %d are ",f1,f2); 
-
-           for(i=f1; i<=f2; ++i){
-
-               rem = i % 2;
-
-               if(rem == 0)
-
-               printf("\n  %d",i);
-
-           }
-
-    return 0; 
+int main()
+{
+ int m,n,rem,i;
+ printf("First Range:\n");
+ scanf("%d",&m);
+ printf("Final Range:\n");
+ scanf("%d",&n);
+ printf("\n The Even numbers between the given interval  %d and%d are",m,n); 
+ for(i=m;i<=n;++i)
+{
+ rem=i%2;
+ if(rem==0)
+ printf("\n %d",i);
+ return 0; 
+ }}
