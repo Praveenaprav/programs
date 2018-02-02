b72aa686d03688e59e29db02399b6442abeb8adb
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-02 16:55:03 +0530

    Create countchar.c

---
+++ countchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int length,n1,n2,n3;
+char a[50],b[50],c[50];
+scanf("%s %s %s",&a,&b,&c);
+n1=strlen(a);
+n2=strlen(b);
+n3=strlen(c);
+length=n1+n2+n3;
+printf("length is:%d",length);
+return 0;
+}
