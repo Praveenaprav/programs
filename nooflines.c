ce038e891fd3e3b5375509076b50a022579c6e87
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-05 16:47:39 +0530

    Create nooflines.c

---
+++ nooflines.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int k,i;
+char a[50];
+printf("enter the paragraph:");
+scanf("%s",&a[i]);
+k=strlen(a);
+printf("no of lines in the given paragraph is:",k);
+return 0;
+}
