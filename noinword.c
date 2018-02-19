e345941b3490667f52936605cce8ef0d53f8bcd3
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-19 16:24:17 +0530

    Create noinword.c

---
+++ noinword.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int ch;
+printf("\n enter ur choice:");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:one;
+       break;
+case 2:two;
+       break;
+case 3:three;
+       break;
+case 4:four;
+       break;
+case 5:five;
+       break;
+case 6:six;
+       break;
+case 7:seven;
+       break;
+case 8:eight;
+       break;
+case 9:nine;
+       break;
+case 10:ten;
+       break;
+}
+return 0;
+}
