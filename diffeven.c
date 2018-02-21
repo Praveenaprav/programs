2fec0e06cad3cf0522a2526fa2b83c35de15ffad
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-21 16:54:09 +0530

    Create diffeven.c

---
+++ diffeven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int m,n,c;
+scanf("%d %d",&m,&n);
+c=m-n;
+printf("\n",c);
+if(c%2==0)
+printf("\n difference is even");
+else
+printf("\n difference is odd");
+return 0;
+}
