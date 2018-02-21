5ea9525485a05f4bc76d41ac66f5c2eaaa926ebb
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-21 15:44:40 +0530

    Create muleven.c

---
+++ muleven.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d %d",&a,&b);
+c=a*b;
+printf("\n",c);
+if(c%2==0)
+printf("\n multiplication is even");
+else
+printf("\n multiplication is odd");
+return 0;
+}
+
