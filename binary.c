6ed96d211be1ee2f6f1139cc6204dd94a723a775
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-02-26 16:51:03 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+unsigned a;
+printf("\n enter the string:");
+scanf("%i",&a);
+if(a%10==0|a%10==1)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
