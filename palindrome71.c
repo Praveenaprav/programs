0eeeda18ca79ccba57c8afe8ae25e29e1a4b11a8
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-03-02 15:46:37 +0530

    Create palindrome71.c

---
+++ palindrome71.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ int main()
+ {
+ int s1,s2;
+ printf("\n enter the string:");
+ scanf("%s",&s1);
+ s2=strrev(s1);
+ if(s2==s1)
+ printf("\n yes");
+ else
+ printf("\n no");
+ return 0;
+ }
+
