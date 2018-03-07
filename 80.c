4b40f8f8bc9ceb187fe29c0a301c60be8aceac2a
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-03-07 16:18:56 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,rev=0,rem;
+scanf("%d",&n);
+while(n!=0)
+{
+rem=n%10;
+rev=(rev*10)+rem;
+n=n/10;
+}
+n=rev;
+while(n!=0)
+{
+rem=n%10;
+if(rem%2==0)
+printf(" ");
+else
+printf("%d",rem);
+}
+n=n/10;
+return 0;
+}
