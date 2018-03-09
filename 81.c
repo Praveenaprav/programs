d045721f0e8b0e19fb01ffd1b883be6a6453724d
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-03-09 15:52:08 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a1,a2,a3,b1,b2,b3,c1,c2,c3;
+printf("\n enter the a1 value:");
+scanf("%d",&a1);
+printf("\n enter the b1 value:");
+scanf("%d",&b1);
+c1=b1-a1;
+printf("\n enter the a2 value:");
+scanf("%d",&a2);
+printf("\n enter the b2 value:");
+scanf("%d",&b2);
+c2=b2-a2;
+printf("\n enter the a3 value:");
+scanf("%d",&a3);
+printf("\n enter the b3 value:");
+scanf("%d",&b3);
+c3=b3-a3;
+printf("\n %d",c1);
+printf("\n %d",c2);
+printf("\n %d",c3);
+return 0;
+}
