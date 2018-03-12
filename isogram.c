f28477b53d6a983168da9c3c98fe3b912bfd55e9
Praveenaprav <30748556+Praveenaprav@users.noreply.github.com>
2018-03-12 15:39:25 +0530

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	char op;
+	printf("\n enter the values:");
+	scanf("%d %c %d",&a,&op,&b);
+	switch(op)
+	{
+		case '%':
+		c=a%b;
+		printf("\n %d",c);
+		break;
+		case '/':
+		c=a/b;
+		printf("%d",c);
+		break;
+		default:
+		printf("");
+	}return 0;
+}
+		
